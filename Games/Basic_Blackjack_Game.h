f8c00a816e1fe05e3bb1c3bbc6e7f82d8bf44662
Simon Clements <simon.clements@bluefruit.co.uk>
2015-11-17 00:11:06 +0000

    Started to flesh-out Blackjack class methods, using Game base-class

--- Games/Basic_Blackjack_Game.h
+++ Games/Basic_Blackjack_Game.h
@@ -1,13 +1,24 @@
 #ifndef BASIC_BLACKJACK_INCLUDED
 #define BASIC_BLACKJACK_INCLUDED
 
+#include "BasicDeck.h"
+#include "BasicCard.h"
 #include "Game.h"
+#include <map>
 
 class BlackJackGame : public Game
 {
 public:
+	BlackJackGame();
+	~BlackJackGame();
+	BlackJackGame(const BlackJackGame&) = default;
+	BlackJackGame& operator = (const BlackJackGame& rVal);
+	void AddPlayer(const std::string& playerName, int deckSize = 0);
+	void PlayerDrawCard(BasicDeck* deck);
+	BasicDeck* Player(const std::string& playerName);
+	size_t CurrentNumberOfPlayers() const;
 private:
-
+	std::map<std::string, BasicDeck*> m_players;
 };
 
 #endif
